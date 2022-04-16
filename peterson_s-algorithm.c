47465e39c985005fc1680d57715b939f02c608b6
Peter Yu <peteryu24@naver.com>
2022-04-16 17:43:48 +0900

    two processes / mutual exclusion

--- peterson_s-algorithm.c
+++ peterson_s-algorithm.c
@@ -1,26 +1,16 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <pthread.h>
 
- 
 int cnt;
-
 int flag[2] = {0,0};
-
-int turn = 0;
-
- 
+int turn = 0; 
 
 void *func0() {
 
     for (int i = 0; i < 100000; i++) {
-
         flag[0]=1; // 0번째 프로세스가 임계영역을 사용하겠다고 알림
-
         turn=1; // 어떤 프로세스를 실행시킬건지 가르키는 변수
-
         while(flag[1] == 1 && turn ==1){} // busy waits
                                           /*
                                           turn == 1일 경우 내 차례가 아닌데 자원까지 사용하려고 한다면
@@ -33,9 +23,7 @@ void *func0() {
                                           */
 
             cnt++; // 카운트 증가
-
             printf("cnt1 :: %d\n", cnt);  //출력
-
             flag[0]=0; // 사용종료. false 로 교체
     }
 
@@ -44,17 +32,12 @@ void *func0() {
 void *func1() {
 
     for (int i = 0; i < 100000; i++) {
-
         flag[1]=1;
-
         turn=0;
-
-        while(flag[0] == 1 && turn ==0){}
-            
+        
+      while(flag[0] == 1 && turn ==0){        
             cnt++;
-
             printf("cnt2 :: %d\n", cnt);
-
             flag[1]=0;       
 
     }
@@ -65,24 +48,15 @@ void *func1() {
 int main()
 
 {
-
      pthread_t thread1, thread2;
-
      int  iret1, iret2;
-
      iret1 = pthread_create( &thread1, NULL, func0, NULL);
-
      iret2 = pthread_create( &thread2, NULL, func1, NULL);
 
-
      pthread_join( thread1, NULL);
-
      pthread_join( thread2, NULL); 
 
-
      printf("Thread error: %d\n", 200000-cnt);
-
      exit(0);
-
 }
 
